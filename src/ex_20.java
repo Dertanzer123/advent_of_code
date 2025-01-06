9f90e1229c2770b5899b82f5f2ee55f2bb1e3fe2
Emin Tekin <emint2719gmail.com>
2025-01-06 22:44:58 +0300

    20th part towo

--- src/ex_20.java
+++ src/ex_20.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 
 public class ex_20 {
 
-    ex_20() throws IOException {
         int startX = 0;
         int startY = 0;
         int endX = 0;
@@ -19,6 +18,7 @@ public class ex_20 {
         int width = 0;
         int height = 0;
         ArrayList<int[]> map = new ArrayList<>();
+    ex_20() throws IOException {
 
         String path = "src/inputs/day20.txt";
         BufferedReader reader = new BufferedReader(new FileReader(path));
@@ -89,6 +89,124 @@ public class ex_20 {
         }
 
         int filternumero=100;
+
+        System.out.println(goodCheatNumber20p(filternumero));
+
+
+//        System.out.println(map.get(startY)[startX]);
+//        for(int[] i:map)
+//        {
+//            for(int j:i)
+//            {
+//               if(j==-1)
+//               {
+//                   System.out.print("##");
+//               }
+//               else if(j==-2)
+//               {
+//                   System.out.print("..");
+//               }
+//               else
+//               {
+//                   if(j<10)
+//                   {
+//                       System.out.print("_");
+//                   }
+//                   System.out.print(j);
+//
+//               }
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+
+
+
+    }
+    long goodCheatNumber20p(int filternumero)
+    {
+        long desantCheatNumero=0;
+        while (currentX != endX || currentY != endY)
+        {
+            for(int i=2;i<=20;i++)
+            {
+                int dx=i;
+                int dy=0;
+                for(int k=0;k<i;k++)
+                {
+
+                    if(currentX+dx<width&&currentX+dx>=0&&currentY+dy<height&&currentY+dy>=0&&map.get(currentY+dy)[currentX+dx]!=-1&&map.get(currentY)[currentX]-map.get(currentY+dy)[currentX+dx]>=filternumero+i)
+                    {
+                        desantCheatNumero++;
+                    }
+                    dx--;
+                    dy++;
+                }
+                for(int k=0;k<i;k++)
+                {
+
+                    if(currentX+dx<width&&currentX+dx>=0&&currentY+dy<height&&currentY+dy>=0&&map.get(currentY+dy)[currentX+dx]!=-1&&map.get(currentY)[currentX]-map.get(currentY+dy)[currentX+dx]>=filternumero+i)
+                    {
+                        desantCheatNumero++;
+                    }
+                    dx--;
+                    dy--;
+                }
+                for(int k=0;k<i;k++)
+                {
+
+                    if(currentX+dx<width&&currentX+dx>=0&&currentY+dy<height&&currentY+dy>=0&&map.get(currentY+dy)[currentX+dx]!=-1&&map.get(currentY)[currentX]-map.get(currentY+dy)[currentX+dx]>=filternumero+i)
+                    {
+                        desantCheatNumero++;
+                    }
+                    dx++;
+                    dy--;
+                }
+                for(int k=0;k<i;k++)
+                {
+
+                    if(currentX+dx<width&&currentX+dx>=0&&currentY+dy<height&&currentY+dy>=0&&map.get(currentY+dy)[currentX+dx]!=-1&&map.get(currentY)[currentX]-map.get(currentY+dy)[currentX+dx]>=filternumero+i)
+                    {
+                        desantCheatNumero++;
+                    }
+                    dx++;
+                    dy++;
+                }
+
+
+
+
+            }
+            if( currentX>0&&map.get(currentY)[currentX-1]!=-1&&map.get(currentY)[currentX-1]<map.get(currentY)[currentX])
+            {
+
+                currentX--;
+
+            }
+            else if(currentX<width-1&&map.get(currentY)[currentX+1]!=-1&&map.get(currentY)[currentX+1]<map.get(currentY)[currentX])
+            {
+
+                currentX++;
+            }
+            else if(currentY>0&&map.get(currentY-1)[currentX]!=-1&&map.get(currentY-1)[currentX]<map.get(currentY)[currentX])
+            {
+
+                currentY--;
+
+            }
+            else if(currentY<height-1&&map.get(currentY+1)[currentX]!=-1&&map.get(currentY+1)[currentX]<map.get(currentY)[currentX])
+            {
+
+                currentY++;
+            }
+        }
+        return desantCheatNumero;
+    }
+    int goodCheatNumber2p(int filternumero)
+    {
         int desantCheatNumero=0;
         while (currentX != endX || currentY != endY)
         {
@@ -136,42 +254,9 @@ public class ex_20 {
                 currentY++;
             }
         }
-        System.out.println(desantCheatNumero);
-
-
-//        System.out.println(map.get(startY)[startX]);
-//        for(int[] i:map)
-//        {
-//            for(int j:i)
-//            {
-//               if(j==-1)
-//               {
-//                   System.out.print("##");
-//               }
-//               else if(j==-2)
-//               {
-//                   System.out.print("..");
-//               }
-//               else
-//               {
-//                   if(j<10)
-//                   {
-//                       System.out.print("_");
-//                   }
-//                   System.out.print(j);
-//
-//               }
-//            }
-//            System.out.println();
-//        }
-
-
-
-
-
-
-
+        return desantCheatNumero;
     }
+
 
 
 }
